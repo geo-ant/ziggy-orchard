409eac28283f20cd081e330b1a4572dbd6fb6be0
geo-ant <54497890+geo-ant@users.noreply.github.com>
2021-06-12 22:50:21 +0200

    metaprogramming concepts / traits experiments

--- src/dice.zig
+++ src/dice.zig
@@ -1,4 +1,27 @@
 const std = @import("std");
+const assert = comptime std.debug.assert;
+const TypeInfo = std.builtin.TypeInfo;
+
+/// a concept-like / trait like metafunction to
+/// check at compile time whether a given argument's type satisfies the
+/// prng interface necessary for generating a random dice throw
+fn assertIsOfPrngType(arg : anytype) void {
+    const prng_type = comptime switch (@typeInfo(@TypeOf(arg))) {
+        .Struct => @TypeOf(arg),
+        .Pointer => |ptr| ptr.child,
+        else => {@compileError("Type of PRNG must be pointer or struct");}
+    };
+
+    const hasFieldRandom = std.meta.trait.hasField("random");
+    comptime if (!hasFieldRandom(prng_type)) {
+        @compileError("PRNG must have member field 'random'");
+    };
+
+    const hasRngFunction = std.meta.trait.hasFn("intRangeAtMost");
+    comptime if (!hasFieldRandom(prng_type)) {
+        @compileError("PRNG member field 'random' must have member function 'intRangeAtMost'");
+    };
+} 
 
 /// A variant that contains a valid dice result, either
 /// * a raven
@@ -21,9 +44,20 @@ pub const DiceResult = union(enum) {
         return @This(){ .fruit = try Fruit.new(index) };
     }
 
-    pub fn new_random(prng :  * std.rand.DefaultPrng) @This() {
+    /// generate a new random dice result using a prng
+    /// # Arguments
+    /// prng: must be a random number generator which has a member
+    /// random and that member must have a function intRangeAtMost(comptime T:type,lower:T, upper:T)
+    /// which generates an integer in the range [lower, upper].
+    /// TODO: I am checking the type at compile time to provide better error messages, 
+    /// but I don't know if I am doing it idiomatically.
+    pub fn new_random(prng :  anytype) @This() {
+        // this is to check the arg for better error messages
+        assertIsOfPrngType(prng);
+
         const rand = prng.random.intRangeAtMost(u8, 0, Fruit.TREE_COUNT+1);
-        
+
+
         switch (rand) {
             0...Fruit.TREE_COUNT-1 => |index| return .{.fruit = Fruit.new(index) catch  unreachable},
             Fruit.TREE_COUNT => return .{.basket = .{}},
@@ -54,3 +88,8 @@ const DiceError = error{
     /// invalid fruit index was given 
     InvalidFruitIndex
 };
+
+
+test "something" {
+
+}
